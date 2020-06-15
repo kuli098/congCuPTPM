5a7c21ee89772301edb63cbf992a0db8bf335690
VanPhat <vanphathutechit@gmail.com>
2020-06-15 16:16:01 +0700

    thêm file cấu hình cho dự án

---
+++ config/dev.js
@@ -0,0 +1,16 @@
+module.exports = {
+  DATABASE: 'mongodb://localhost:27017/DoAnTongHop',
+  JWT_SECRET: 'myjsonwebtoken',
+  JWT_TIME: '90d',
+  SENDGRIB_KEY:
+    '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',
+  DOMAIN: 'http://localhost:5000',
+  HOST_SEND_MAIL: 'http://localhost:3000',
+  GOOGLE_CLIENT_ID:
+    '523445467595-1s859kbqipdgc6fk3eotco8bj64iubl6.apps.googleusercontent.com',
+  GOOGLE_CILENT_SECRET: 'yychMjvcwWS-femR3-tWMNwq',
+  FACEBOOK_CLIENT_ID: '163245805129876',
+  FACEBOOK_CILENT_SECRET: '0826848b560914609e16d593248065b1',
+  TOKEN_CHATBOT: '1080399519:AAENr5mvkjQrvlRVwRl0AHaUl9P4dFtGTKc',
+  CHATBOT_CHATID: '639081333',
+};
