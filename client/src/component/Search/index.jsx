28a42ae51993d3ff35e7b248ae00906e722abfd4
huuvinhdocthan <huuvinhit98@gmail.com>
2020-06-15 16:39:39 +0700

    vinh: tao giao dien da mua & hoc khoa hoc

---
+++ client/src/component/Search/index.jsx
@@ -0,0 +1,14 @@
+import { Toolbar } from '@material-ui/core';
+import React from 'react';
+import SearchContent from '../Layout/SearchContent';
+
+const index = () => {
+  return (
+    <>
+      <Toolbar />
+      <SearchContent />
+    </>
+  );
+};
+
+export default index;
