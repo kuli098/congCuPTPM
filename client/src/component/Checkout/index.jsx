d6087c3e90fde8964882f494a244f39b9b0cac39
huuvinhdocthan <huuvinhit98@gmail.com>
2020-06-15 16:33:04 +0700

    vinh: tao giao dien login them gio hang

---
+++ client/src/component/Checkout/index.jsx
@@ -0,0 +1,13 @@
+import { Toolbar } from '@material-ui/core';
+import React from 'react';
+import Payment from '../Layout/Payment';
+
+const index = () => {
+  return (
+    <>
+      <Toolbar />
+      <Payment />
+    </>
+  );
+};
+export default index;
